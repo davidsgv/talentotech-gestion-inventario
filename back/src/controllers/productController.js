e04e01c1db53a2971d469a72bfb77cc446eb1fd7
DavidSG <davidsgv98@gmail.com>
2024-10-10 01:04:29 +0000

    feat: :sparkles: Create and Find Products

--- back/src/controllers/productController.js
+++ back/src/controllers/productController.js
@@ -1,31 +1,52 @@
-const Producto = require("../models/product")
+const Product = require("../models/product")
 const History = require("../models/stockHistory")
 const Category = require("../models/category")
 
 exports.CreateProduct = async (req, res) => {
-    // try{
-    //     let category = await Category.findOne({name: req.body.category})
-    //     if(!category){
-    //         category = new Category({name: req.body.category})
-    //     }
-    //     console.log(category)
+    try{
+        let category = await Category.findOne({name: req.body.category})
+        if(!category){
+            category = new Category({name: req.body.category})
+            await category.save()
+        }
+        console.log(category)
 
-    //     const newProduct = new Producto(req.body)
-    //     newProduct.category = category._id;
-    //     const newHistoryObj = {
-    //         name: newProduct.name,
-    //         description: newProduct.description,
-    //         stock: newProduct.stock
-    //     }
-    //     const newHistory = new History(newHistoryObj);
+        const newProduct = new Product(req.body)
+        newProduct.category = category._id;
+        const newHistoryObj = {
+            name: newProduct.name,
+            description: newProduct.description,
+            stock: newProduct.stock,
+            category: req.body.category
+        }
+        const newHistory = new History(newHistoryObj);
 
-    //     await newProduct.save()
-    //     await newHistory.save()
+        await newProduct.save()
+        await newHistory.save()
 
-    //     res.status(201).json(newProduct)
-    // } catch (error){
-    //     res.status(500).json({error: error.message})
-    // }
+        res.status(201).json(newProduct)
+    } catch (error){
+        res.status(500).json({error: error.message})
+    }
+}
 
-    res.status(201).json({message: "new product"})
+exports.GetProducts = async (req, res) =>{
+    try{
+        const products = await Product.find().populate("category");
+        res.status(200).json(products);
+    } catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+exports.GetProductById = async (req, res)=>{
+    try{
+        const product = await Product.findById(req.params.id).populate("category");
+        if(!product){
+            return res.status(404).json({message: "Producto no encontrado"});
+        }
+        res.status(200).json(product);
+    } catch(error){
+        res.status(500).json({error: error.message});
+    }
 }
--- back/src/routes/product.js
+++ back/src/routes/product.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/productController")
 
 router.post("/", controller.CreateProduct);
+router.get("/", controller.GetProducts);
+router.get("/:id", controller.GetProductById);
 
 module.exports = router;
