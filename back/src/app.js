871f76f964f420e8efeb7672a9512ffc3cdf83f9
DavidSG <davidsgv98@gmail.com>
2024-10-10 01:46:17 +0000

    chore: :art: add support for .env file in root

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
-require("dotenv").config();
+require("dotenv").config({ path: '../.env' });
 
 const productoRouter = require("./routes/product")
 
